46da2567bd6055021e131dd4abcb93921b151afd
Dima <dimaspider2012@gmail.com>
2025-03-21 16:58:58 +0200

    commit 4

--- main2.js
+++ main2.js
@@ -1,4 +1,5 @@
 const btnauthor = document.querySelector(".btn-author");
+
 btnauthor.addEventListener("click", () => {
   window.location.href = "3.html";
 });
@@ -8,31 +9,58 @@ btnauthor.addEventListener("click", () => {
 Зима: https://facts.cx.ua/uploads/facts-2/mini/cikavi-fakti-pro-zimu-800-450.jpg */
 const scriptUrl =
   "https://script.google.com/macros/s/AKfycbyvPYQgKwEqaL1qeKelNyqffgKXEhuqbh-TvFtlQADALVoyAfhaO9viKghX2DrjZZggaQ/exec";
+let fetchedDataPeople = [];
+let fetchedDataSchedule = [];
+
 async function fetchData() {
   try {
     const response = await fetch(scriptUrl);
     if (!response.ok) throw new Error("Network response was not ok");
     const data = await response.json();
     console.log(data);
-    const fetchedData = data.slice(1, -1);
-    displayData(fetchedData);
+    fetchedDataPeople = data.slice(1, 21);
+    fetchedDataSchedule = data.slice(23, 29);
   } catch (error) {
     console.error("Error", error);
   }
 }
 
-function displayData(data) {
-  const tableBody = document.getElementById("data-table");
+function showColumn(columnIndex) {
+  const tableBody = document.querySelector("#data-table tbody"); // Оновлено
+  const nameheader = document.querySelector("#name-header");
+  nameheader.style.display = "table-cell";
+  if (!tableBody) {
+    console.error("Table body not found!");
+    return;
+  }
+
   tableBody.innerHTML = "";
-  data.forEach((row) => {
+  fetchedDataPeople.forEach((row) => {
     const tr = document.createElement("tr");
-    row.forEach((cell) => {
-      const td = document.createElement("td");
-      td.textContent = cell;
-      tr.appendChild(td);
-    });
+    const td = document.createElement("td");
+    td.textContent = row[columnIndex] || "—"; // Додаємо перевірку, якщо даних нема
+    tr.appendChild(td);
     tableBody.appendChild(tr);
   });
 }
 
-// fetchData();
+function showColumnSchedule(columnIndex) {
+  const tableBody = document.querySelector("#data-table1 tbody"); // Оновлено
+  const nameheader = document.querySelector("#name-header1");
+  nameheader.style.display = "table-cell";
+  if (!tableBody) {
+    console.error("Table body not found!");
+    return;
+  }
+
+  tableBody.innerHTML = "";
+  fetchedDataSchedule.forEach((row) => {
+    const tr = document.createElement("tr");
+    const td = document.createElement("td");
+    td.textContent = row[columnIndex] || "—"; // Додаємо перевірку, якщо даних нема
+    tr.appendChild(td);
+    tableBody.appendChild(tr);
+  });
+}
+
+fetchData();
