ccd1d832e627b62114f948c5dc243e0ab5eb4fc8
Dima <dimaspider2012@gmail.com>
2025-04-02 15:25:34 +0300

    commit 13

--- main1.5.js
+++ main1.5.js
@@ -1,1 +1,9 @@
- 
+const btn6 = document.querySelector(".btn6");
+const btn7 = document.querySelector(".btn7");
+const btn8 = document.querySelector(".btn8");
+const btn9 = document.querySelector(".btn9");
+const btn10 = document.querySelector(".btn10");
+const btn11 = document.querySelector(".btn11");
+btn6.addEventListener("click", () => {
+  window.location.href = "2.html";
+});
