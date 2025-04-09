949599e03e544c08363c879e77f14f4579920281
Dima <dimaspider2012@gmail.com>
2025-04-09 15:19:03 +0300

    commit 15

--- main1.js
+++ main1.js
@@ -3,7 +3,7 @@ const password = document.getElementById("password");
 const btnlogin = document.querySelector(".btn-login");
 btnlogin.addEventListener("click", () => {
   if (login.value === "admin" && password.value === "123") {
-    window.location.href = "2.html";
+    window.location.href = "index2.html";
   } else {
     console.log("error");
   }
--- main2.js
+++ main2.js
@@ -1,8 +1,8 @@
 const btnauthor = document.querySelector(".btn-author");
 
-btnauthor.addEventListener("click", () => {
-  window.location.href = "3.html";
-});
+// btnauthor.addEventListener("click", () => {
+//   window.location.href = "index3.html";
+// });
 /* Весна: https://i.pinimg.com/736x/01/1d/c4/011dc4dd931d658d9fc69e7747f3aff0.jpg
 Осінь: https://tsystem.com.ua/wp-content/uploads/2024/09/autumn.jpg
 Літо: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7mvY1EEAbcnTHdJZOxQVJysNkbaOyVFU06g&s
