ca13f3f426435b14108ee152e777d513154f8951
Dima <dimaspider2012@gmail.com>
2025-05-07 15:25:03 +0300

    version 3

--- main2.js
+++ main2.js
@@ -3,6 +3,29 @@ const btnauthor = document.querySelector(".btn-author");
 // btnauthor.addEventListener("click", () => {
 //   window.location.href = "index3.html";
 // });
+
+const bg = {
+  spring:
+    "https://i.pinimg.com/736x/01/1d/c4/011dc4dd931d658d9fc69e7747f3aff0.jpg",
+  summer:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7mvY1EEAbcnTHdJZOxQVJysNkbaOyVFU06g&s",
+  autumn: "https://tsystem.com.ua/wp-content/uploads/2024/09/autumn.jpg",
+  winter:
+    "https://facts.cx.ua/uploads/facts-2/mini/cikavi-fakti-pro-zimu-800-450.jpg",
+};
+
+function getSeasonByData(date = new Date()) {
+  const month = date.getMonth();
+  if (month >= 2 && month <= 4) return "spring";
+  if (month >= 5 && month <= 7) return "summer";
+  if (month >= 8 && month <= 10) return "autumn";
+  return "winter";
+}
+const currentSeason = getSeasonByData();
+const currentBg = bg[currentSeason];
+console.log(currentSeason);
+console.log(currentBg);
+document.body.style.backgroundImage = `url(${currentBg})`;
 /* Весна: https://i.pinimg.com/736x/01/1d/c4/011dc4dd931d658d9fc69e7747f3aff0.jpg
 Осінь: https://tsystem.com.ua/wp-content/uploads/2024/09/autumn.jpg
 Літо: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7mvY1EEAbcnTHdJZOxQVJysNkbaOyVFU06g&s
